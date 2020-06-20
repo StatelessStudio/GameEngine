b5e0ccab4a94072cc0700fc025957228fc4c98c6
Drew Immerman <drewimmerman@gmail.com>
2020-06-20 15:17:15 -0400

    Add Entity::id

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,11 +1,29 @@
 #include "Entity.h"
+#include "Exception.h"
 
 namespace SSGE
 {
 
 Entity::Entity() :
+	id(0),
+	hasId(false),
 	scale(Vec3(1, 1, 1))
 {
+}
+
+unsigned int Entity::getId()
+{
+	if (hasId) {
+		throw SSGE::Exception("ID has not been set for entity");
+	}
+
+	return id;
+}
+
+void Entity::setId(unsigned int _id)
+{
+	id = _id;
+	hasId = true;
 }
 
 Vec3 Entity::getScale()
--- src/Entity.h
+++ src/Entity.h
@@ -12,11 +12,32 @@ namespace SSGE
 class Entity : public Node
 {
 protected:
+	/** ID */
+	unsigned long id;
+
+	/** ID can only be received if set */
+	bool hasId;
+
 	/** Scale */
 	Vec3 scale;
 
 public:
 	Entity();
+
+	/**
+	 * Get ID
+	 * 
+	 * @return {unsigned int} Returns the entity id
+	 * @throws SSGE::Exception if the value has not been set
+	 */
+	unsigned int getId();
+
+	/**
+	 * Set ID
+	 * 
+	 * @param {unsigned int} id New ID
+	 */
+	void setId(unsigned int _id);
 
 	/**
 	 * Get scale
