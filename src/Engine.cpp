9448b4e6fe5b63653ab8b1db7d8c37e6808b0dd3
Drew Immerman <drewimmerman@gmail.com>
2020-09-27 18:27:31 -0400

    Move shared Engine thread code to GameEngine library

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,6 +1,11 @@
 #include "Engine.h"
 
+#include <chrono>
 #include <iostream>
+
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::system_clock;
 
 namespace SSGE
 {
@@ -8,11 +13,19 @@ namespace SSGE
 Engine::Engine()
 {
 	std::cout << "Engine Started!" << std::endl;
+	shouldRun = true;
 }
 
 Engine::~Engine()
 {
 	std::cout << "Engine Teardown!" << std::endl;
+}
+
+unsigned __int64 Engine::getTime()
+{
+	return duration_cast<milliseconds>(
+		system_clock::now().time_since_epoch()
+	).count();
 }
 
 } // namespace SSGE
--- src/Engine.h
+++ src/Engine.h
@@ -1,13 +1,29 @@
 #pragma once
 
+#include <Poco/Thread.h>
+
 namespace SSGE
 {
 
 class Engine
 {
+protected:
+	bool isStopped = false;
+	Poco::Thread thread;
+
 public:
+	bool shouldRun;
+	int tickTimeMs = 30;
+
 	Engine();
 	~Engine();
+
+	/**
+	 * Get epoch time in milliseconds
+	 *
+	 * @return Time in milliseconds
+	 */
+	unsigned __int64 getTime();
 };
 
 } // namespace SSGE
